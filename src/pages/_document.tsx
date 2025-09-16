161444fad44b435d7dbbccf83d5c16fa55cd3ae7
SHINMENTAKEZO <INVINCIBLESHINMEN@GMAIL.COM>
2025-09-16 21:35:03 +0000

    fix: convert _document.tsx to class component for Next.js compatibility

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,21 +1,30 @@
-import { Html, Head, Main, NextScript } from 'next/document'
- 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        {/* Preload CSS with correct attributes */}
-        <link
-          rel="preload"
-          href="/_next/static/css/37059c639290d4c0.css"
-          as="style"
-          crossOrigin="anonymous"
-        />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {/* Preload CSS with correct attributes */}
+          <link
+            rel="preload"
+            href="/_next/static/css/37059c639290d4c0.css"
+            as="style"
+            crossOrigin="anonymous"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
